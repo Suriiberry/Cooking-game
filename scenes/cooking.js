8af36635b3ce57a0cace3cba6caf24bb00d164db
Suriiberry <nanamichan1215@gmail.com>
2026-06-18 11:50:55 -0400

    Update cooking.js

--- scenes/cooking.js
+++ scenes/cooking.js
@@ -1,1 +1,27 @@
+// scenes/cooking.js
+import Phaser from 'phaser';
+import { Order } from '../utils/order';
 
+class Cooking extends Phaser.Scene {
+  constructor() {
+    super('Cooking');
+  }
+
+  preload() {}
+
+  create() {
+    // Add cooking station graphics
+    this.cookingStation = this.add.sprite(200, 300, 'cooking-station');
+
+    // Add player character
+    this.player = this.physics.add.sprite(400, 500, 'player');
+
+    // Create order object
+    const menuItem = new Order('Vegetable Stir Fry', 2);
+    this.order = menuItem;
+  }
+
+  update(time, delta) {
+    // Implement cooking mechanics (e.g., timing-based mini-game)
+  }
+}
