e14594ef318273b6d0f4e1d4dcabfcf84b530fb4
Suriiberry <nanamichan1215@gmail.com>
2026-06-18 11:50:18 -0400

    Update order.js

--- utils/order.js
+++ utils/order.js
@@ -1,1 +1,13 @@
+// utils/order.js
+class Order {
+  constructor(menuItem, quantity) {
+    this.menuItem = menuItem;
+    this.quantity = quantity;
+  }
 
+  get totalCost() {
+    return this.menuItem.price * this.quantity;
+  }
+}
+
+export default Order;
