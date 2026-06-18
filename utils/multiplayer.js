fa31b217da61850dabfb7eb339536d1d3e9f5d3a
Suriiberry <nanamichan1215@gmail.com>
2026-06-18 11:51:18 -0400

    Update multiplayer.js

--- utils/multiplayer.js
+++ utils/multiplayer.js
@@ -1,1 +1,19 @@
+// utils/multiplayer.js
+class Multiplayer {
+  constructor() {
+    this.players = [];
+  }
 
+  addPlayer(player) {
+    this.players.push(player);
+  }
+
+  removePlayer(player) {
+    const index = this.players.indexOf(player);
+    if (index !== -1) {
+      this.players.splice(index, 1);
+    }
+  }
+
+  update(time, delta) {}
+}
